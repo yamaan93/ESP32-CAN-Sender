cca3a7a3439430c152366ed0420e5850bdf44189
yamaan93 <yamaan93@gmail.com>
2022-11-17 10:16:48 -0500

    initial commit; send sample data over CAN

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <mcp2515.h>
+
+MCP2515 mcp2515(2); // cs/ss pin is 5 
+
+struct can_frame canMessage;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SPI.begin();
+   mcp2515.reset();
+   mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
+   mcp2515.setNormalMode();
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int speed = 14;
+  int temp = 25;
+
+  canMessage.can_id = 0x000;
+  canMessage.can_dlc = 4;
+  canMessage.data[0] = speed;
+  canMessage.data[1] = temp;
+
+  mcp2515.sendMessage(&canMessage);
+
+
 }
